d36bf3b768b89d0e596741d08a8622b49db260d9
Farhana Najnin mahi <129548085+Farhana-Najnin@users.noreply.github.com>
2024-06-30 00:37:05 +0600

    Update settings.py

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ['https://mamar-bank.onrender.com','https://*.127.0.0.1']
 
 # Application definition
